aab79c14b8097c5fa1560a5bf7d0a0dae21a3577
ZEO2011 <hatemziad384@gmail.com>
2024-01-19 17:07:17 +0200

    added codeium AI completion

--- lua/thegreatagen/lazy.lua
+++ lua/thegreatagen/lazy.lua
@@ -88,6 +88,16 @@ local plugins = {
         -- See `:help lualine.txt`
         opts = require('configs.lualine')
     },
+    {
+        'Exafunction/codeium.vim',
+        config = function()
+            -- Change '<C-g>' here to any keycode you like.
+            vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+        end
+    },
 }
 
 require("lazy").setup(plugins, {})
