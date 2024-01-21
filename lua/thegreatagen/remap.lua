1302ea7245244c09dc548c66a40b743761ca161d
thegreatagen1 <hatemziad384@gmail.com>
2024-01-21 09:32:46 +0200

    added keybind to save the file

--- lua/thegreatagen/remap.lua
+++ lua/thegreatagen/remap.lua
@@ -8,6 +8,7 @@ end)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
