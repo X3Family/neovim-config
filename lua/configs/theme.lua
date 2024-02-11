9c77b4bfd19845e074567558e9ffde433535c3e9
thegreatagen1 <hatemziad384@gmail.com>
2024-02-11 06:43:00 +0200

    fixed errors
    Signed-off-by: thegreatagen1 <hatemziad384@gmail.com>

--- lua/configs/theme.lua
+++ lua/configs/theme.lua
@@ -1,15 +1,9 @@
-require('onedark').setup({
-    function_style = "italic",
-    sidebars = { "qf", "vista_kind", "terminal", "packer" },
-
-    colors = { hint = "orange0", error = "#ff0000" },
-
-    overrides = function(c)
-        return {
-            htmlTag = { fg = c.red0, bg = "#282c34", sp = c.hint, style = "underline" },
-            DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
-            -- this will remove the highlight groups
-            TSField = {},
-        }
-    end
-})
+local theme = {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("catppuccin");
+	end
+};
+return theme;
--- lua/thegreatagen/lazy.lua
+++ lua/thegreatagen/lazy.lua
@@ -1,168 +1,187 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim";
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath
+	});
+end;
+vim.opt.rtp:prepend(lazypath);
+local theme = require("configs.theme");
 local plugins = {
-    -- lazy.nvim plugin ( the package manager )
-    "folke/which-key.nvim",
-    { "folke/neoconf.nvim",              cmd = "Neoconf" },
-    "folke/neodev.nvim",
-    { 'nvim-treesitter/nvim-treesitter', name = 'tree-sitter' },
-    -- nvim tree
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-    },
-    -- mason
-    {
-        "williamboman/mason.nvim"
-    },
-
-    -- theme
-    { "ful1e5/onedark.nvim" , name="onedarkTheme"},
-    -- lsps
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/cmp-cmdline' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
-
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-
-        }
-    },
-
-    -- comments.nvim
-    {
-        'numToStr/Comment.nvim',
-        opts = require('configs.comment'),
-        config = function()
-            require('Comment').setup()
-        end,
-    },
-
-    -- presence
-    { "andweeb/presence.nvim" },
-    -- ibl
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-    -- autoclose
-    { "m4xshen/autoclose.nvim" },
-    -- -- Harpoon
-    { "nvim-lua/plenary.nvim" },
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { { "nvim-lua/plenary.nvim" } }
-    },
-    -- telescope
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        -- or                              , branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    -- lualine
-    {
-        -- Set lualine as statusline
-        'nvim-lualine/lualine.nvim',
-        -- See `:help lualine.txt`
-        opts = require('configs.lualine'),
-    },
-    -- codeium AI
-    {
-        'Exafunction/codeium.vim',
-        config = function()
-            -- Change '<C-g>' here to any keycode you like.
-            vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-        end
-    },
-
-    -- alpha
-    {
-        'goolord/alpha-nvim',
-    },
-
-    -- trouble
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    -- live share
-    {
-        "jbyuki/instant.nvim",
-        config = function()
-            vim.g.instant_username = "Ahmedx3"
-        end
-    },
-
-    -- persistence
-    {
-        "folke/persistence.nvim",
-        event = "BufReadPre", -- this will only start session saving when an actual file was opened
-        opts = {
-            -- add any custom options here
-        }
-    },
-
-    -- nvim-ts-autotag
-    { "windwp/nvim-ts-autotag" },
-
-    -- todo-comments
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    },
-
-    { "tribela/vim-transparent", config = function() vim.g.transparent_enabled = true end },
-    { "wuelnerdotexe/vim-astro", config = function() vim.g.astro_typescript = true end },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup()
-            vim.cmd.colorscheme("catppuccin")
-            vim.g.catppuccin_flavour = "mocha"
-        end
-    },
-    theme
-}
-
-
-require("lazy").setup(plugins, {})
+	"folke/which-key.nvim",
+	{
+		"folke/neoconf.nvim",
+		cmd = "Neoconf"
+	},
+	"folke/neodev.nvim",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		name = "tree-sitter"
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons"
+		}
+	},
+	{
+		"williamboman/mason.nvim"
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		dependencies = {
+			{
+				"neovim/nvim-lspconfig"
+			},
+			{
+				"williamboman/mason.nvim"
+			},
+			{
+				"williamboman/mason-lspconfig.nvim"
+			},
+			{
+				"hrsh7th/nvim-cmp"
+			},
+			{
+				"hrsh7th/cmp-buffer"
+			},
+			{
+				"hrsh7th/cmp-path"
+			},
+			{
+				"hrsh7th/cmp-cmdline"
+			},
+			{
+				"saadparwaiz1/cmp_luasnip"
+			},
+			{
+				"hrsh7th/cmp-nvim-lsp"
+			},
+			{
+				"hrsh7th/cmp-nvim-lua"
+			},
+			{
+				"L3MON4D3/LuaSnip"
+			},
+			{
+				"rafamadriz/friendly-snippets"
+			}
+		}
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = require("configs.comment"),
+		config = function()
+			(require("Comment")).setup();
+		end
+	},
+	{
+		"andweeb/presence.nvim"
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {}
+	},
+	{
+		"m4xshen/autoclose.nvim"
+	},
+	-- Harpoon
+	{ "nvim-lua/plenary.nvim" },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { { "nvim-lua/plenary.nvim" } }
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		}
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = require("configs.lualine")
+	},
+	{
+		"Exafunction/codeium.vim",
+		config = function()
+			vim.keymap.set("i", "<C-g>", function()
+				return vim.fn["codeium#Accept"]();
+			end, {
+				expr = true
+			});
+			vim.keymap.set("i", "<c-;>", function()
+				return vim.fn["codeium#CycleCompletions"](1);
+			end, {
+				expr = true
+			});
+			vim.keymap.set("i", "<c-,>", function()
+				return vim.fn["codeium#CycleCompletions"](-1);
+			end, {
+				expr = true
+			});
+			vim.keymap.set("i", "<c-x>", function()
+				return vim.fn["codeium#Clear"]();
+			end, {
+				expr = true
+			});
+		end
+	},
+	{
+		"goolord/alpha-nvim"
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons"
+		}
+	},
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		opts = {}
+	},
+	{
+		"windwp/nvim-ts-autotag"
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		},
+		opts = {}
+	},
+	{
+		"tribela/vim-transparent",
+		config = function()
+			vim.g.transparent_enabled = true;
+		end
+	},
+	{
+		"wuelnerdotexe/vim-astro",
+		config = function()
+			vim.g.astro_typescript = true;
+		end
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			(require("catppuccin")).setup();
+			vim.cmd.colorscheme("catppuccin");
+			vim.g.catppuccin_flavour = "mocha";
+		end
+	},
+	theme
+};
+(require("lazy")).setup(plugins, {});
