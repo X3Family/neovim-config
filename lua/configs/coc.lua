ea7cb3df36a2601f8f7b52807798353c3b48f8e9
thegreatagen1 <hatemziad384@gmail.com>
2024-03-05 18:48:20 +0200

    fixed some issues

--- lua/configs/coc.lua
+++ lua/configs/coc.lua
@@ -1,7 +1,4 @@
 local lazyCoc = "%USERPROFILE%\\AppData\\Local\\"
-vim.fn.system("cd " .. lazyCoc .. "nvim-data\\lazy\\coc.nvim")
-vim.fn.system("npm install")
-
 
 local coc_plugins = {
    "coc-discord-rpc",
@@ -10,7 +7,7 @@ local coc_plugins = {
 for _, plugin in ipairs(coc_plugins) do
    local result = vim.fn.system(
       "dir /ad " .. lazyCoc .. "coc\\extensions\\node_modules\\" .. plugin)
-   if string.find(result, "File Not Found") ~= nil then
+   if string.find(result, "The system cannot find the file specified.") ~= nil then
       vim.cmd("CocInstall " .. plugin)
    end
 end
